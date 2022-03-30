29eba5f180e26af5e56d2d74ba0708781e399ce1
514K <alekseysafronov0@gmail.com>
2022-03-30 05:21:33 -0400

    Add hello

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ print("""Content-Type: text/html; charset=utf-8 \n\n
 	    }
 	  </script>
 	</head>
-	<body onload="userTime()"> <p id="userTime"> </p> </body>""")
+	<body onload="userTime()"> Hello <p id="userTime"> </p> </body>""")
